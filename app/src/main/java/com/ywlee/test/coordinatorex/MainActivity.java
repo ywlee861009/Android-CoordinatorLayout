c67d61a34199dfcabb332820f0c19ace4ab29955
ywlee861009 <ywlee861009@gmail.com>
2018-08-03 22:09:10 +0900

    databinding 적용

--- app/src/main/java/com/ywlee/test/coordinatorex/MainActivity.java
+++ app/src/main/java/com/ywlee/test/coordinatorex/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ywlee.test.coordinatorex;
 
+import android.databinding.DataBindingComponent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ywlee.test.coordinatorex.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //  Blocked for DataBinding
+//        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+
+        binding.txvDatabinding.setText("adfsdf");
     }
 }
